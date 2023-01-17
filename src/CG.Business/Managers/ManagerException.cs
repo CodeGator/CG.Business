a63989c3b0259382bc4369524910f9d6e03aa071
Martin Cook <33993749+martycook@users.noreply.github.com>
2023-01-17 12:52:16 -0600

    refactoring

--- src/CG.Business/Managers/ManagerException.cs
+++ src/CG.Business/Managers/ManagerException.cs
@@ -1,75 +1,72 @@
-﻿using System;
-using System.Runtime.Serialization;
+﻿
+namespace CG.Business.Managers;
 
-namespace CG.Business.Managers
+/// <summary>
+/// This class represents a manager related exception.
+/// </summary>
+[Serializable]
+public class ManagerException : Exception
 {
+    // *******************************************************************
+    // Constructors.
+    // *******************************************************************
+
+    #region Constructors
+
     /// <summary>
-    /// This class represents a manager related exception.
+    /// This constructor creates a new instance of the <see cref="ManagerException"/>
+    /// class.
     /// </summary>
-    [Serializable]
-    public class ManagerException : BusinessException
+    public ManagerException()
     {
-        // *******************************************************************
-        // Constructors.
-        // *******************************************************************
 
-        #region Constructors
-
-        /// <summary>
-        /// This constructor creates a new instance of the <see cref="ManagerException"/>
-        /// class.
-        /// </summary>
-        public ManagerException()
-        {
-
-        }
-
-        // *******************************************************************
-
-        /// <summary>
-        /// This constructor creates a new instance of the <see cref="ManagerException"/>
-        /// class.
-        /// </summary>
-        /// <param name="message">The message to use for the exception.</param>
-        /// <param name="innerException">An optional inner exception reference.</param>
-        public ManagerException(
-            string message,
-            Exception innerException
-            ) : base(message, innerException)
-        {
-
-        }
-
-        // *******************************************************************
-
-        /// <summary>
-        /// This constructor creates a new instance of the <see cref="ManagerException"/>
-        /// class.
-        /// </summary>
-        /// <param name="message">The message to use for the exception.</param>
-        public ManagerException(
-            string message
-            ) : base(message)
-        {
-
-        }
-
-        // *******************************************************************
-
-        /// <summary>
-        /// This constructor creates a new instance of the <see cref="ManagerException"/>
-        /// class.
-        /// </summary>
-        /// <param name="info">The serialization info to use for the exception.</param>
-        /// <param name="context">The streaming context to use for the exception.</param>
-        public ManagerException(
-            SerializationInfo info,
-            StreamingContext context
-            ) : base(info, context)
-        {
-
-        }
-
-        #endregion
     }
+
+    // *******************************************************************
+
+    /// <summary>
+    /// This constructor creates a new instance of the <see cref="ManagerException"/>
+    /// class.
+    /// </summary>
+    /// <param name="message">The message to use for the exception.</param>
+    /// <param name="innerException">An optional inner exception reference.</param>
+    public ManagerException(
+        string message,
+        Exception innerException
+        ) : base(message, innerException)
+    {
+
+    }
+
+    // *******************************************************************
+
+    /// <summary>
+    /// This constructor creates a new instance of the <see cref="ManagerException"/>
+    /// class.
+    /// </summary>
+    /// <param name="message">The message to use for the exception.</param>
+    public ManagerException(
+        string message
+        ) : base(message)
+    {
+
+    }
+
+    // *******************************************************************
+
+    /// <summary>
+    /// This constructor creates a new instance of the <see cref="ManagerException"/>
+    /// class.
+    /// </summary>
+    /// <param name="info">The serialization info to use for the exception.</param>
+    /// <param name="context">The streaming context to use for the exception.</param>
+    public ManagerException(
+        SerializationInfo info,
+        StreamingContext context
+        ) : base(info, context)
+    {
+
+    }
+
+    #endregion
 }
--- src/CG.Business/Repositories/RepositoryException.cs
+++ src/CG.Business/Repositories/RepositoryException.cs
@@ -1,75 +1,72 @@
-﻿using System;
-using System.Runtime.Serialization;
+﻿
+namespace CG.Business.Repositories;
 
-namespace CG.Business.Repositories
+/// <summary>
+/// This class represents a repository related exception.
+/// </summary>
+[Serializable]
+public class RepositoryException : Exception
 {
+    // *******************************************************************
+    // Constructors.
+    // *******************************************************************
+
+    #region Constructors
+
     /// <summary>
-    /// This class represents a repository related exception.
+    /// This constructor creates a new instance of the <see cref="RepositoryException"/>
+    /// class.
     /// </summary>
-    [Serializable]
-    public class RepositoryException : BusinessException
+    public RepositoryException()
     {
-        // *******************************************************************
-        // Constructors.
-        // *******************************************************************
 
-        #region Constructors
-
-        /// <summary>
-        /// This constructor creates a new instance of the <see cref="RepositoryException"/>
-        /// class.
-        /// </summary>
-        public RepositoryException()
-        {
-
-        }
-
-        // *******************************************************************
-
-        /// <summary>
-        /// This constructor creates a new instance of the <see cref="RepositoryException"/>
-        /// class.
-        /// </summary>
-        /// <param name="message">The message to use for the exception.</param>
-        /// <param name="innerException">An optional inner exception reference.</param>
-        public RepositoryException(
-            string message,
-            Exception innerException
-            ) : base(message, innerException)
-        {
-
-        }
-
-        // *******************************************************************
-
-        /// <summary>
-        /// This constructor creates a new instance of the <see cref="RepositoryException"/>
-        /// class.
-        /// </summary>
-        /// <param name="message">The message to use for the exception.</param>
-        public RepositoryException(
-            string message
-            ) : base(message)
-        {
-
-        }
-
-        // *******************************************************************
-
-        /// <summary>
-        /// This constructor creates a new instance of the <see cref="RepositoryException"/>
-        /// class.
-        /// </summary>
-        /// <param name="info">The serialization info to use for the exception.</param>
-        /// <param name="context">The streaming context to use for the exception.</param>
-        public RepositoryException(
-            SerializationInfo info,
-            StreamingContext context
-            ) : base(info, context)
-        {
-
-        }
-
-        #endregion
     }
+
+    // *******************************************************************
+
+    /// <summary>
+    /// This constructor creates a new instance of the <see cref="RepositoryException"/>
+    /// class.
+    /// </summary>
+    /// <param name="message">The message to use for the exception.</param>
+    /// <param name="innerException">An optional inner exception reference.</param>
+    public RepositoryException(
+        string message,
+        Exception innerException
+        ) : base(message, innerException)
+    {
+
+    }
+
+    // *******************************************************************
+
+    /// <summary>
+    /// This constructor creates a new instance of the <see cref="RepositoryException"/>
+    /// class.
+    /// </summary>
+    /// <param name="message">The message to use for the exception.</param>
+    public RepositoryException(
+        string message
+        ) : base(message)
+    {
+
+    }
+
+    // *******************************************************************
+
+    /// <summary>
+    /// This constructor creates a new instance of the <see cref="RepositoryException"/>
+    /// class.
+    /// </summary>
+    /// <param name="info">The serialization info to use for the exception.</param>
+    /// <param name="context">The streaming context to use for the exception.</param>
+    public RepositoryException(
+        SerializationInfo info,
+        StreamingContext context
+        ) : base(info, context)
+    {
+
+    }
+
+    #endregion
 }
